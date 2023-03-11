366cd66bb3f06438bdd6710f191238360d3a5b8f
Alan Fernandez <alan.f@msn.com>
2023-03-11 11:12:30 -0300

    PosterViewer updates

--- components/PosterViewer/PosterViewer.tsx
+++ components/PosterViewer/PosterViewer.tsx
@@ -1,8 +1,10 @@
-import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
-import { RootState } from "@/state/store";
-import Modal from "../Modal/Modal";
-import useModal from "@/hooks/useModal";
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '@/state/store';
+import Modal from '../Modal/Modal';
+import useModal from '@/hooks/useModal';
+
+import { PosterContainer, PosterImage } from './styledComponents';
 
 interface PosterViewerTypes {}
 
@@ -11,13 +13,22 @@ const PosterViewer = (props: PosterViewerTypes) => {
   const currentPoster = useSelector(
     (state: RootState) => state.shopify.currentPoster
   );
+  const { title, images } = currentPoster
+    ? currentPoster
+    : { title: '', images: [{ src: '' }] };
 
   useEffect(() => {
-    setIsOpen(!!currentPoster?.id)
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [currentPoster])
+    setIsOpen(!!currentPoster?.id);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [currentPoster]);
 
-  return <Modal isOpen={isOpen} setIsOpen={setIsOpen}><p>{currentPoster?.name}</p></Modal>;
+  return (
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <PosterContainer>
+        <PosterImage alt={title} src={images[0].src} width={100} height={100} />
+      </PosterContainer>
+    </Modal>
+  );
 };
 
 export default PosterViewer;
